2409c6a2caaf1a21a972f7c8c2625c8d64aee04d
David <104197044+DavidCaidas@users.noreply.github.com>
2022-10-06 18:31:16 +0200

    ut01ex05 actividad planteada y con cambios hechos con ayuda porqur yo solo no he podido ni de coña

--- app/src/main/java/com/davidcai/aad/ut01/ex05/data/remote/UsersRemoteDataSource.kt
+++ app/src/main/java/com/davidcai/aad/ut01/ex05/data/remote/UsersRemoteDataSource.kt
@@ -8,20 +8,17 @@ class UsersRemoteDataSource  {
      * Mock
      * Replace with getUser from PSP
      */
-    fun getUsers(): List<User>{
-
-        return mutableListOf(
-            User(1, "User1", "Username1"),
-            User(2, "User2", "Username2"),
-            User(3, "User3", "Username3"),
-            User(4, "User4", "Username4"),
-            User(5, "User5", "Username5"),
-            User(6, "User6", "Username6"),
-            User(7, "User7", "Username7"),
-        )
+    fun getUsers():List<User> = mutableListOf(
+        User (1, "User1", "Username1"),
+        User (2, "User2", "Username2"),
+        User (3, "User3", "Username3"),
+        User (4, "User4", "Username4"),
+        User (5, "User5", "Username5"),
+        User (6, "User6", "Username6"),
+        User (7, "User7", "Username7")
+    )
     }
 
     fun getUser(userId: Int) : User{
         return User(userId, "User$userId", "Username$userId")
     }
-}
